5b9f69ec40ea302b15200a7cefc49be20c3e9175
Jordon Baade <github@jordonbaade.com>
2020-12-30 14:28:35 -0500

    Initial Commit

---
+++ webpack.mix.js
@@ -0,0 +1,18 @@
+const mix = require('laravel-mix');
+require('laravel-mix-jigsaw');
+
+mix.disableSuccessNotifications();
+mix.setPublicPath('source/assets/build');
+
+mix.js('source/_assets/js/main.js', 'js')
+    .jigsaw({
+        watch: ['config.php', 'source/**/*.md', 'source/**/*.php', 'source/**/*.css'],
+    })
+    .options({
+        processCssUrls: false,
+        postCss: [
+            require('tailwindcss'),
+        ],
+    })
+    .sourceMaps()
+    .version();
